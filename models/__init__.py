0d8242ce0b69842a285fe5fee9b19370f0854cc9
Maude Lizaire <maude_liz@hotmail.ca>
2024-08-07 14:34:26 -0400

    adding models

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from .cp_rnn import CPRNN
+from .second_order_rnn import SecondOrderRNN
+from .mirnn import MIRNN
+from .birnn import BiRNN
+from .rnn import RNN
+from .bicprnn import BiCPRNN
---
+++ models/second_order_rnn.py
@@ -0,0 +1,129 @@
+from typing import Union
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+from features.tokenizer import CharacterTokenizer
+
+
+class SecondOrderRNN(nn.Module):
+    """Second Order RNN. Outputs logits (no softmax)
+
+    Args:
+        input_size: Dimension of input features.
+        hidden_size: Dimension of hidden features.
+        vocab_size: Size of vocabulary
+        use_embedding: Whether to use embedding layer or one-hot encoding
+        tokenizer: Character tokenizer
+        batch_first: Whether to use batch first or not
+        dropout: Dropout rate
+
+    """
+    def __init__(self, input_size: int, hidden_size: int, vocab_size: int, use_embedding: bool = False,
+                 gate: str = 'tanh', tokenizer: CharacterTokenizer = None, batch_first: bool = True,
+                 dropout: float = 0.5, **kwargs):
+        super().__init__()
+
+        self.use_embedding = use_embedding
+        self.dropout = dropout
+        self.batch_first = batch_first
+        self.tokenizer = tokenizer
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.gate = {"tanh": torch.tanh, "sigmoid": torch.sigmoid, "identity": lambda x: x}[gate]
+
+        # Define embedding and decoder layers
+        if use_embedding:
+            self.embedding = nn.Embedding(self.vocab_size, self.input_size)
+
+        else:
+            # One hot version
+            self.embedding = lambda x: torch.nn.functional.one_hot(x, vocab_size).float()
+            self.input_size = vocab_size
+
+        self.decoder = nn.Sequential(
+            nn.Dropout(self.dropout),
+            nn.Linear(self.hidden_size, self.vocab_size)
+        )
+
+        # Encoder using CP factors
+        self.w = nn.Parameter(torch.Tensor(self.hidden_size + 1, self.input_size + 1, self.hidden_size))
+        self.init_weights()
+
+
+    def init_weights(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def init_hidden(self, batch_size, device=torch.device('cpu')):
+        h = torch.zeros(batch_size, self.hidden_size).to(device)
+        return h
+
+    def predict(self, inp: Union[torch.LongTensor, str], init_states: tuple = None, top_k: int = 1,
+                device=torch.device('cpu')):
+
+        with torch.no_grad():
+
+            if isinstance(inp, str):
+                if self.tokenizer is None:
+                    raise ValueError("Tokenizer not defined. Please provide a tokenizer to the model.")
+                x = torch.tensor(self.tokenizer.char_to_ix(inp)).reshape(1, 1).to(device)
+            else:
+                x = inp.to(device)
+
+            output, init_states = self.forward(x, init_states)
+            output_conf = torch.softmax(output, dim=-1)  # [S, B, Din]
+            output_topk = torch.topk(output_conf, top_k, dim=-1)  # [S, B, K]
+
+            prob = output_topk[0].reshape(-1) / output_topk[0].reshape(-1).sum()
+            k_star = np.random.choice(np.arange(top_k), p=prob.cpu().numpy())
+            output_ids = output_topk[1][:, :, k_star]
+
+            if isinstance(inp, str):
+                output_char = self.tokenizer.ix_to_char(output_ids.item())
+                return output_char, init_states
+            else:
+                return output_ids, init_states
+
+    def forward(self, inp: torch.LongTensor, init_states: torch.Tensor = None):
+
+        # print("In gpu {} | Shape: {}".format(torch.cuda.current_device(), inp.shape))
+
+        if self.batch_first:
+            inp = inp.transpose(0, 1)
+
+        if len(inp.shape) != 2:
+            raise ValueError("Expected input tensor of order 2, but got order {} tensor instead".format(len(inp.shape)))
+
+        x = self.embedding(inp)  # [S, B, D_in] (i.e. [sequence, batch, input_size])
+        sequence_length, batch_size,  _ = x.size()
+        hidden_seq = []
+
+        device = x.device
+
+        if init_states is None:
+            h_t = torch.zeros(batch_size, self.hidden_size).to(x.device)
+
+        else:
+            h_t = init_states
+            h_t = h_t.to(device)
+
+        for t in range(sequence_length):
+            x_t = x[t, :, :]  # [B, D_in]
+
+            h_prime = torch.cat((h_t, torch.ones(batch_size, 1).to(device)), dim=1)  # [B, D_h][D_h, R] => [B, R]
+            x_prime = torch.cat((x_t, torch.ones(batch_size, 1).to(device)), dim=1)   # [B, D_i'][D_i', R] => [B, R]
+            h_t = self.gate(torch.einsum("bi,bj,ijk->bk", h_prime, x_prime, self.w))
+            hidden_seq.append(h_t.unsqueeze(0))
+
+        hidden_seq = torch.cat(hidden_seq, dim=0)
+        output = self.decoder(hidden_seq.contiguous())
+
+        if self.batch_first:
+            output = output.transpose(0, 1)
+
+        return output, h_t
